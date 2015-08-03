63207c61321bf5493597c712916511895cc732ab
Fermín Olaiz <fermino.github@gmail.com>
2015-08-03 11:47:52 -0300

    Backup.php added (main script)

---
+++ Backup.php
@@ -0,0 +1,28 @@
+<?php
+	require_once 'class/Lib/Std.php';
+
+	require_once 'class/CPanelBackupper.php';
+
+	try
+	{
+		Std::Out();
+		Std::Out('== CPanel Backupper ==');
+		Std::Out('Maquinola.Net <contacto@maquinola.net>');
+
+		$CPanelBackupper = new CPanelBackupper;
+
+		Std::Out();
+		Std::Out('Generating backups...', 2);
+
+		$Backups = $CPanelBackupper->Backup();
+
+		foreach($Backups as $Username => $Success)
+			Std::Out("    {$Username} = " . var_export($Success));
+	}
+	catch(Exception $Exception)
+	{
+		Std::Out();
+		Std::Out("Exception: {$Exception->GetMessage()}");
+
+		exit;
+	}
